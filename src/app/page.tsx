4e6fe9f04b71fd63c15bac5f17f45541e8f9f71a
sabareesh-s <sabareesh1221@gmail.com>
2023-06-29 16:54:05 +0530

    added nextauth and google sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+'use client';
+import { signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+  const session = useSession();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello world
+    <main className="">
+      {session?.data?.user?.name}
     </main>
   )
 }
